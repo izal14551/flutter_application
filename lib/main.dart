6ccc387c2057245836d807d705cca3439457997e
izal14551 <94549806+izal14551@users.noreply.github.com>
2024-04-23 18:52:27 +0700

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-          backgroundColor: Color.fromARGB(255, 255, 115, 0),
-        ),
-        backgroundColor: Color.fromARGB(255, 243, 33, 191),
-        body: Center(
-          child: Text('Halo Saya Fadel Alif Afrizal'),
+      title: 'My Profile',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+/*            AspectRatio(
+              aspectRatio: 16,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.asset('assets/images/DSC_0325.jpg'),
+              ),
+            ),*/
+
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              padding: const EdgeInsets.all(2),
+              child: const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/DSC_0325.JPG'),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Fadel Alif Afrizal',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Jl. Sunan Kalijaga, Berkoh, Purwokerto Selatan',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
         ),
       ),
     );
